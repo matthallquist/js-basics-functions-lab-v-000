125c1f72bc2d71113d1ec5392e757679c8c13962
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-27 17:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(value) {
-  let result;
-  result = 42 - value;
+  let result = 42 - value;
   if (Math.sign(result) === -1) {
     result = -result;
   }
